b8e684321235664caedeaca684cb9fdba2981811
DevanshuSinghai <devanshusinghai2005@gmail.com>
2024-05-21 14:17:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# discord_clone
+ discord frontend using html and tailwind css
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        ggs: [""]
+      },
+      colors:{
+        bg_blue: "#404EED"
+      }
+    },
+  },
+  plugins: [],
+}
+
